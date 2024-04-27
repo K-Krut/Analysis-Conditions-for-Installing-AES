048d433e22acd2d1d07d10b7ed166e3c8518f3a8
K-Krut <katiakrut448@gmail.com>
2024-04-27 14:42:07 +0300

    added detailed landscape classification

--- area_analysis_api/constants.py
+++ area_analysis_api/constants.py
@@ -82,6 +82,167 @@ landscape_types = {
     200: "Oceans, seas. Can be either fresh or salt-water bodies."
 }
 
+
+landscape_types_details = [
+    {
+        "id": 0,
+        "name": "Unknown",
+        "details": "Unknown. No or not enough satellite data available.",
+        "suitable": False
+    },
+    {
+        "id": 20,
+        "name": "Shrubs",
+        "details": "Shrubs. Woody perennial plants with persistent and woody stems and without any defined main stem "
+                   "being less than 5 m tall. The shrub foliage can be either evergreen or deciduous.",
+        "suitable": False
+    },
+    {
+        "id": 30,  # ?
+        "name": "Herbaceous vegetation",
+        "details": "Herbaceous vegetation. Plants without persistent stem or shoots above ground and lacking definite "
+                   "firm structure. Tree and shrub cover is less than 10 %.",
+        "suitable": False
+    },
+    {
+        "id": 40,
+        "name": "Crops",
+        "details": "Cultivated and managed vegetation / agriculture. Lands covered with temporary crops followed by "
+                   "harvest and a bare soil period (e.g., single and multiple cropping systems). Note that perennial "
+                   "woody crops will be classified as the appropriate forest or shrub land cover type.",
+        "suitable": True
+    },
+    {
+        "id": 50,
+        "name": "Urban",
+        "details": "Urban / built up. Land covered by buildings and other man-made structures.",
+        "suitable": False
+    },
+    {
+        "id": 60,
+        "name": "Bare / sparse vegetation",
+        "details": "Bare / sparse vegetation. Lands with exposed soil, sand, or rocks and never has more than 10 % "
+                   "vegetated cover during any time of the year.",
+        "suitable": True
+    },
+    {
+        "id": 70,
+        "name": "Snow and ice",
+        "details": "Snow and ice. Lands under snow or ice cover throughout the year.",
+        "suitable": False
+    },
+    {
+        "id": 80,
+        "name": "Permanent water bodies",
+        "details": "Permanent water bodies. Lakes, reservoirs, and rivers. Can be either fresh or salt-water bodies.",
+        "suitable": False
+    },
+    {
+        "id": 90,
+        "name": "Herbaceous wetland",
+        "details": "Herbaceous wetland. Lands with a permanent mixture of water and herbaceous or woody vegetation. "
+                   "The vegetation can be present in either salt, brackish, or fresh water.",
+        "suitable": False
+    },
+    {
+        "id": 100,
+        "name": "Moss and lichen.",
+        "details": "Moss and lichen.",
+        "suitable": True
+    },
+    {
+        "id": 111,
+        "name": "Closed forest",
+        "details": "Closed forest, evergreen needle leaf. Tree canopy >70 %, almost all needle leaf trees remain "
+                   "green all year. Canopy is never without green foliage.",
+        "suitable": False
+    },
+    {
+        "id": 112,
+        "name": "Closed forest",
+        "details": "Closed forest, evergreen broad leaf. Tree canopy >70 %, almost all broadleaf trees remain green "
+                   "year round. Canopy is never without green foliage.",
+        "suitable": False
+    },
+    {
+        "id": 113,
+        "name": "Closed forest",
+        "details": "Closed forest, deciduous needle leaf. Tree canopy >70 %, consists of seasonal needle leaf tree "
+                   "communities with an annual cycle of leaf-on and leaf-off periods.",
+        "suitable": False
+    },
+    {
+        "id": 114,
+        "name": "Closed forest",
+        "details": "Closed forest, deciduous broad leaf. Tree canopy >70 %, consists of seasonal broadleaf "
+                   "tree communities with an annual cycle of leaf-on and leaf-off periods.",
+        "suitable": False
+    },
+    {
+        "id": 115,
+        "name": "Closed forest, mixed.",
+        "details": "Closed forest, mixed.",
+        "suitable": False
+    },
+    {
+        "id": 116,
+        "name": "Closed forest",
+        "details": "Closed forest, not matching any of the other definitions.",
+        "suitable": False
+    },
+    {
+        "id": 121,
+        "name": "Open forest",
+        "details": "Open forest, evergreen needle leaf. Top layer- trees 15-70 % and second layer- mixed of shrubs and "
+                   "grassland, almost all needle leaf trees remain green all year."
+                   "Canopy is never without green foliage.",
+        "suitable": False
+    },
+    {
+        "id": 122,
+        "name": "Open forest, evergreen broad leaf",
+        "details": "Open forest, evergreen broad leaf. Top layer- trees 15-70 % and second layer- mixed of shrubs "
+                   "and grassland, almost all broadleaf trees remain green year round. "
+                   "Canopy is never without green foliage.",
+        "suitable": False
+    },
+    {
+        "id": 123,
+        "name": "Open forest, deciduous needle leaf",
+        "details": "Open forest, deciduous needle leaf. Top layer- trees 15-70 % and second layer- mixed of shrubs "
+                   "and grassland, consists of seasonal needle leaf tree communities with "
+                   "an annual cycle of leaf-on and leaf-off periods.",
+        "suitable": False
+    },
+    {
+        "id": 124,
+        "name": "Open forest, deciduous needle leaf",
+        "details": "Open forest, deciduous broad leaf. Top layer- trees 15-70 % and second layer- mixed of shrubs and "
+                   "grassland, consists of seasonal broadleaf tree communities with "
+                   "an annual cycle of leaf-on and leaf-off periods.",
+        "suitable": False
+    },
+    {
+        "id": 125,
+        "name": "Open forest, mixed.",
+        "details": "Open forest, mixed.",
+        "suitable": False
+    },
+    {
+        "id": 126,
+        "name": "Open forest",
+        "details": "Open forest, not matching any of the other definitions.",
+        "suitable": False
+    },
+    {
+        "id": 200,
+        "name": "Oceans, seas",
+        "details": "Oceans, seas. Can be either fresh or salt-water bodies.",
+        "suitable": False
+    },
+]
+
+
 COPERNICUS_CROP_CLASS_ID = 40
 
 FILTERING_AREAS_SCALE = 300
