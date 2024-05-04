93d7af5839fbb699e76b384bd68d8a4def4df47b
Katia Krut <katiakrut488@gmail.com>
2024-05-04 15:06:55 +0300

    preparation for deploy

--- area_analysis_api/views.py
+++ area_analysis_api/views.py
@@ -7,11 +7,6 @@ from diploma_api import settings
 from .utils import get_ee_classification
 
 
-def map_view(request):
-    context = {
-        'google_maps_api_key': settings.GOOGLE_MAPS_API_KEY
-    }
-    return render(request, 'index.html', context)
 
 
 class AnalyzeArea(APIView):
