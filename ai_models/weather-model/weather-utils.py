ac8b56ddd8fdf598fa12e779e5bb0640a5467017
Katia Krut <katiakrut488@gmail.com>
2024-05-06 22:06:17 +0300

    getting stats weather data

--- ai_models/weather-model/weather-utils.py
+++ ai_models/weather-model/weather-utils.py
@@ -1,22 +1,26 @@
-from builtins import print
-
+from diploma_api.settings import RapidAPI_KEY
+from datetime import datetime, timedelta
 import requests
 
-from diploma_api.settings import RapidAPI_KEY
 
-url = "https://meteostat.p.rapidapi.com/point/monthly"
-
-querystring = {"lat": 50.330228, "lon": 26.239297, "start": "2023-01-01", "end": "2024-01-01"}
-
-headers = {
-    "X-RapidAPI-Key": RapidAPI_KEY,
-    "X-RapidAPI-Host": "meteostat.p.rapidapi.com"
-}
-
-response = requests.get(url, headers=headers, params=querystring)
-
-print(response.json())
+def get_date_start(date_end):
+    previous_date = datetime.strptime(date_end, '%Y-%m-%d') - timedelta(days=30 * 11)
+    return previous_date.replace(day=1).strftime('%Y-%m-%d')
 
 
+def get_last_year_weather_data(coordinates, date_end):
+    response = requests.get(
+        "https://meteostat.p.rapidapi.com/point/monthly",
+        headers={
+            "X-RapidAPI-Key": RapidAPI_KEY,
+            "X-RapidAPI-Host": "meteostat.p.rapidapi.com"
+        },
+        params={
+            "lat": coordinates[0],
+            "lon": coordinates[1],
+            "start": get_date_start(date_end),
+            "end": date_end,
+        }
+    )
 
-##################################################################################################
+    return response.json()
