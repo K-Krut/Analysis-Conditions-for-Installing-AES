5b3c7e450f4ed93014969286bac9a2da7850f4cd
Katia Krut <katiakrut448@gmail.com>
2024-05-26 15:07:07 +0300

    endpoint view updated

--- area_analysis_api/views.py
+++ area_analysis_api/views.py
@@ -2,6 +2,7 @@ from django.http import JsonResponse
 from rest_framework.parsers import JSONParser
 from rest_framework.views import APIView
 
+from ai_models.weather_model.wind_utils import get_wind_energy_output
 from .utils import get_ee_classification, get_solar_energy_output_prediction
 
 
@@ -16,8 +17,19 @@ class AnalyzeArea(APIView):
             if len(coordinates) < 3:
                 return JsonResponse({'error': 'Error - polygon must consist of at least 3 interconnected points'}, status=400)
 
+            try:
+                aes_type = data.get('type')
+            except Exception as e:
+                aes_type = 'solar'
+                print('Error - getting type of analysis\n' + str(e))
+
             landscape_res = get_ee_classification(coordinates)
-            energy_output = get_solar_energy_output_prediction(coordinates[0], landscape_res['crop']) if landscape_res['crop'] else {}
+
+            energy_output = {}
+            if landscape_res['crop']:
+                energy_output = get_wind_energy_output(coordinates[0], landscape_res['initial_polygon_area']) if aes_type == 'wind' \
+                    else get_solar_energy_output_prediction(coordinates[0], landscape_res['crop'])
+
             return JsonResponse(
                 {
                     'coordinates': coordinates,
@@ -26,6 +38,7 @@ class AnalyzeArea(APIView):
                     'initial_polygon_area': landscape_res['initial_polygon_area'],
                     'suitable_polygon_area': landscape_res['suitable_polygon_area'],
                     'energy_output_stats': energy_output,
+                    'type': aes_type,
                 },
                 status=200
             )
